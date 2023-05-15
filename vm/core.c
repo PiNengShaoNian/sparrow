1d3f51f66287f975e418a001a604b959e65fb845
quguiyou <quguiyou1076@qq.com>
2023-05-15 14:21:20 +0800

    UTF8的编码与解码

---
+++ vm/core.c
@@ -0,0 +1,31 @@
+#include "core.h"
+#include <string.h>
+#include <sys/stat.h>
+#include "utils.h"
+#include "vm.h"
+
+char *rootDir = NULL; // 根目录
+
+// 读取源代码文件
+char *readFile(const char *path)
+{
+  FILE *file = fopen(path, "r");
+  if (file == NULL)
+    IO_ERROR("Could't open file \"%s\".", path);
+
+  struct stat fileStat;
+  stat(path, &fileStat);
+  size_t fileSize = fileStat.st_size;
+  char *fileContent = (char *)malloc(fileSize + 1);
+  if (fileContent == NULL)
+    MEM_ERROR("Could't allocate memory for reading file \"%s\".\n", path);
+
+  size_t numRead = fread(fileContent, sizeof(char), fileSize, file);
+  if (numRead < fileSize)
+    IO_ERROR("Could't read file \"%s\".\n", path);
+  fileContent[fileSize] = '\0';
+
+  fclose(file);
+  return fileContent;
+}
+
---
+++ vm/core.h
@@ -0,0 +1,7 @@
+#ifndef _VM_CORE_H
+#define _VM_CORE_H
+
+extern char *rootDir;
+char *readFile(const char *sourceFile);
+
+#endif // _VM_CORE_H
