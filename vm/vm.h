baa9b0699c09f983015992a1c70233379c4e68b2
quguiyou <quguiyou1076@qq.com>
2023-05-16 16:37:32 +0800

    实现list对象

--- vm/vm.h
+++ vm/vm.h
@@ -8,6 +8,7 @@ struct vm
 {
     Class *stringClass;
     Class *fnClass;
+    Class *listClass;
     uint32_t allocatedBytes; // 累计已分配的内存量
     ObjHeader *allObjects;
     Parser *curParser; // 当前此法分析器
