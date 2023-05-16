fe8ef74c9ec75a11c0b6cf43f49fc3a69cdf134f
quguiyou <quguiyou1076@qq.com>
2023-05-16 16:44:36 +0800

    实现range对象

--- vm/vm.h
+++ vm/vm.h
@@ -9,6 +9,7 @@ struct vm
     Class *stringClass;
     Class *fnClass;
     Class *listClass;
+    Class *rangeClass;
     uint32_t allocatedBytes; // 累计已分配的内存量
     ObjHeader *allObjects;
     Parser *curParser; // 当前此法分析器
