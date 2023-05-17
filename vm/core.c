57957e63447c1ba7f614c6f0e9162fe479294bbc
quguiyou <quguiyou1076@qq.com>
2023-05-17 14:10:22 +0800

    定义模块变量

--- vm/core.c
+++ vm/core.c
@@ -45,3 +45,34 @@ void buildCore(VM *vm)
   ObjModule *coreModule = newObjModule(vm, NULL); // NULL为核心模块.name
   mapSet(vm, vm->allModules, CORE_MODULE, OBJ_TO_VALUE(coreModule));
 }
+
+// table中查找符号symbol 找到后返回索引,否则返回-1
+int getIndexFromSymbolTable(SymbolTable *table, const char *symbol, uint32_t length)
+{
+  ASSERT(length != 0, "length of symbol is 0!");
+
+  uint32_t index = 0;
+  while (index < table->count)
+  {
+    if (length == table->datas[index].length &&
+        memcmp(table->datas[index].str, symbol, length) == 0)
+      return index;
+
+    index++;
+  }
+
+  return -1;
+}
+
+// 往table中添加符号symbol,返回其索引
+int addSymbol(VM *vm, SymbolTable *table, const char *symbol, uint32_t length)
+{
+  ASSERT(length != 0, "length if symbol is 0!");
+  String string;
+  string.str = ALLOCATE_ARRAY(vm, char, length + 1);
+  memcpy(string.str, symbol, length);
+  string.str[length] = '\0';
+  string.length = length;
+  StringBufferAdd(vm, table, string);
+  return table->count - 1;
+}
